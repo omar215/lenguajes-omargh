df74ce5c0659c294da3de6b81f5efa995d55051f
T-107 <T-107@PC265>
2015-06-15 19:45:32 -0500

    Clase del 15 de junio

---
+++ src/CheckedExceptions/ProbarChecked.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CheckedExceptions;
+
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class ProbarChecked {
+
+    public static void main(String[] args) throws Exception {
+        
+        File f=new File("archivaldo");
+      
+            FileOutputStream fos=new FileOutputStream(f);
+            ObjectOutputStream oos=new ObjectOutputStream(fos);
+
+        }
+    }
+    
+
---
+++ src/UncheckedExceptions/ProbarExcepciones.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UncheckedExceptions;
+
+public class ProbarExcepciones {
+
+    public static void main(String[] args) {
+        float a=5;
+        int b=0;
+        
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a-b);
+        
+        //Integer i=new Integer("hola");
+        //int otro=5;
+        //System.out.println(i+otro);
+        int []m={2,5,6,7};
+        System.out.println(m[4]);
+    }
+    
+}
